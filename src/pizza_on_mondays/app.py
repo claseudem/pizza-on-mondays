6ad1efce80aa02957919211130bd173e6a376050
claseudem <164520182+claseudem@users.noreply.github.com>
2026-08-12 23:45:31 +0000

    primera version de mi app

---
+++ src/pizza_on_mondays/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+ 
+st.write("""# My first app Hello *world!* """)
+st.write("""# Mi primera app en python """)
+
+
+# TODO: Arreglar los datos de la app
+#df = pd.read_csv("my_data.csv")
+#st.line_chart(df)
